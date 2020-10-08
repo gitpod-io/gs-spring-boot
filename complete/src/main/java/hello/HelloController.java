4bff8d4ecd706be074093b1c13acb417b9f40a63
adorokhin70 <adorokhin@qarik.com>
2020-10-08 15:56:27 +0000

    the test commit

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+
+    public static final String RESULT = "gitpod DEMO for QARIK!";
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return RESULT;
     }
     
 }
